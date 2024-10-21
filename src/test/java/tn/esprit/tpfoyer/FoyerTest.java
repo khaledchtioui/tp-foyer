27b2075d16c4bb1f5f29d809b521ccbae9a2e478
khaledchtioui <khaled.chtioui@esprit.tn>
2024-10-21 17:24:10 +0200

    testFoyerConstructorAndGetters

--- src/test/java/tn/esprit/tpfoyer/FoyerTest.java
+++ src/test/java/tn/esprit/tpfoyer/FoyerTest.java
@@ -1,7 +1,9 @@
-package tn.esprit.tpfoyer.entity;
+package tn.esprit.tpfoyer;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import tn.esprit.tpfoyer.entity.Foyer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FoyerTest {
 
